b19793be38b28210ad335d711be40bd2bfd005f0
Jung JaeWoong <jjw_0609@naver.com>
2023-06-25 21:20:01 +0900

    Feat: fetchContacts 함수 리턴 타입 추가

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,8 @@ interface Contact {
 }
 
 // api
-// TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts() {
-  // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+function fetchContacts(): Promise<Contact[]> {
+  const contacts: Contact[] = [
     {
       name: 'Tony',
       address: 'Malibu',
