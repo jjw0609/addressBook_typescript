8a969ce14b51d3f370b4a866ceaf7f378e28ac47
Jung JaeWoong <jjw_0609@naver.com>
2023-08-06 01:41:02 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -55,13 +55,13 @@ function fetchContacts(): Promise<Contact[]> {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
   constructor() {
     this.fetchData();
   }
 
-  fetchData() {
+  fetchData(): void {
     fetchContacts().then(response => {
       this.contacts = response;
     });
